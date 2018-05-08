0b12d43bf1653bce6998c84bcf4f545e5e22d72f
Hank Gao <hankgao@gmail.com>
2018-05-08 20:48:58 +0800

    Done: list all addresses

---
+++ airdrop/README.md
@@ -0,0 +1,13 @@
+# Overview
+
+bp - blockchain parser
+bp analyzes blockchain database and retrieve information required
+
+## Steps
+
+> Talk to node, rather than write own code.
+
+- Get block from 1 to blockchain head
+- Parse each block to get address
+- 
+
---
+++ airdrop/airdrop.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+
+	"github.com/skycoin/skycoin/src/visor"
+)
+
+const maximumSeq = 10000
+
+func main() {
+	uxpool := getUxPool()
+	addressBook := aggregateAddresses(uxpool)
+
+	i := 0
+	for addr, coins := range addressBook {
+		fmt.Printf("%03d %s=>%f\n", i, addr, coins)
+		i++
+	}
+
+}
+
+func aggregateAddresses(uxpool map[string]visor.ReadableTransactionOutput) map[string]float64 {
+	addressBook := make(map[string]float64)
+
+	for _, ux := range uxpool {
+		coins, err := strconv.ParseFloat(ux.Coins, 64)
+
+		if err != nil {
+			panic(err)
+		}
+
+		addressBook[ux.Address] += coins
+	}
+
+	return addressBook
+}
+
+func getUxPool() map[string]visor.ReadableTransactionOutput {
+	uxpool := make(map[string]visor.ReadableTransactionOutput)
+
+	for i := 1; i <= maximumSeq; i++ {
+		block := getBlock(i)
+
+		for _, t := range block.Body.Transactions {
+			// remove input
+			for _, input := range t.In {
+				delete(uxpool, input)
+			}
+
+			// Record outputs
+			for _, output := range t.Out {
+				uxpool[output.Hash] = output
+			}
+		}
+	}
+
+	return uxpool
+}
+
+func getBlock(which int) visor.ReadableBlock {
+
+	resp, err := http.Get(fmt.Sprintf("http://localhost:6420/block?seq=%d", which))
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	bodyStr, err := ioutil.ReadAll(resp.Body)
+
+	block := visor.ReadableBlock{}
+
+	err = json.Unmarshal(bodyStr, &block)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return block
+
+}
+
+type treasureBookT map[string]uint64
+
+func listAllAddresses() []string {
+	// read the blocks one by one
+
+	var addresses []string
+
+	return addresses
+}
