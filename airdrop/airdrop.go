051e1319f59161cfd7da08990990385d16959432
Hank Gao <hankgao@gmail.com>
2018-05-10 22:09:55 +0800

    add seeds and suncoins accounts

--- airdrop/airdrop.go
+++ airdrop/airdrop.go
@@ -1,97 +1,117 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"strconv"
-
-	"github.com/skycoin/skycoin/src/visor"
+	"strings"
 )
 
-const maximumSeq = 3954
-const webInterfacePort = 7620
-
-func main() {
-	uxpool := getUxPool()
-	addressBook := aggregateAddresses(uxpool)
-
-	i := 0
-	for addr, coins := range addressBook {
-		fmt.Printf("%03d,%s,%f\n", i, addr, coins)
-		i++
-	}
-
+// CsrfValue is something
+type CsrfValue struct {
+	CsrfToken string `json:"csrf_token"`
 }
 
-func aggregateAddresses(uxpool map[string]visor.ReadableTransactionOutput) map[string]float64 {
-	addressBook := make(map[string]float64)
+const (
+	maximumSeq       = 3954
+	webInterfacePort = 8421
+	urlWalletCreate  = "http://127.0.0.1:8421/wallet/create"
+	urlCsrf          = "http://127.0.0.1:8421/csrf"
+	urlWalletBalance = "http://127.0.0.1:8421/wallet/balance"
+)
 
-	for _, ux := range uxpool {
-		coins, err := strconv.ParseFloat(ux.Coins, 64)
-
-		if err != nil {
-			panic(err)
-		}
-
-		addressBook[ux.Address] += coins
-	}
-
-	return addressBook
+type addrItem struct {
+	addr    string
+	balance float64
 }
 
-func getUxPool() map[string]visor.ReadableTransactionOutput {
-	uxpool := make(map[string]visor.ReadableTransactionOutput)
-
-	for i := 1; i <= maximumSeq; i++ {
-		block := getBlock(i)
-
-		for _, t := range block.Body.Transactions {
-			// remove input
-			for _, input := range t.In {
-				delete(uxpool, input)
-			}
-
-			// Record outputs
-			for _, output := range t.Out {
-				uxpool[output.Hash] = output
-			}
-		}
-	}
-
-	return uxpool
-}
-
-func getBlock(which int) visor.ReadableBlock {
-
-	resp, err := http.Get(fmt.Sprintf("http://localhost:%d/block?seq=%d", webInterfacePort, which))
+func getWalletBalance(wn string) (string, string) {
+	resp, err := http.Get(fmt.Sprintf("%s?id=%s", urlWalletBalance, wn))
 	if err != nil {
 		panic(err)
 	}
-
 	defer resp.Body.Close()
 
-	bodyStr, err := ioutil.ReadAll(resp.Body)
+	r := bufio.NewReader(resp.Body)
 
-	block := visor.ReadableBlock{}
+	r.ReadString('\n')
+	r.ReadString('\n')
+	c, _ := r.ReadString('\n')
+	h, _ := r.ReadString('\n')
 
-	err = json.Unmarshal(bodyStr, &block)
+	coins := strings.Split(c, ":")[1]
+	coins = coins[0 : len(coins)-2]
+	hours := strings.Split(h, ":")[1]
+	hours = hours[1 : len(hours)-1]
 
+	fmt.Printf("%s#%s\n", coins, hours)
+
+	return coins, hours
+}
+
+func main() {
+
+	getWalletBalance("distribution002.wlt")
+
+	// book, accounts := readBook("suncoin.airdrop.csv")
+
+	// source pointer, destionation pointer
+	// sp, dp := 0, 0
+	// currentWallet := fmt.Sprintf("distribution%03d.wlt", sp)
+
+	// wb, err := getWalletBalance(currentWallet)
+
+	// check next distribution
+
+}
+
+// CreateDistributionWallets does someting
+// func CreateDistributionWallets() {
+// 	seeds := read100Seeds("suncoin2.100.seeds.csv")
+// 	createDistributionWallets(seeds)
+// }
+
+// ScanSunAddresses get all Suncoin addresses
+// func ScanSunAddresses() {
+// 	uxpool := getUxPool()
+// 	addressBook := aggregateAddresses(uxpool)
+
+// 	i := 0
+// 	for addr, coins := range addressBook {
+// 		fmt.Printf("%03d,%s,%f\n", i, addr, coins)
+// 		i++
+// 	}
+// }
+
+func getCsrfToken() string {
+	csrf := CsrfValue{}
+	resp, err := http.Get(urlCsrf)
 	if err != nil {
 		panic(err)
 	}
 
-	return block
+	data, err := ioutil.ReadAll(resp.Body)
+
+	json.Unmarshal(data, &csrf)
+
+	return csrf.CsrfToken
 
 }
 
-type treasureBookT map[string]uint64
+func getNextTargets(wb float64, book []addrItem) []addrItem {
 
-func listAllAddresses() []string {
-	// read the blocks one by one
+	// book[0].balance
 
-	var addresses []string
+	return []addrItem{}
+}
 
-	return addresses
+func airDoroSun2(book []addrItem) error {
+
+	for _, item := range book {
+		fmt.Println(item)
+	}
+
+	return nil
 }
