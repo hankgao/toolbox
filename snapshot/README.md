ab780c986bce44e38dc27e259f3f29cb0ead9418
Hank Gao <hankgao@gmail.com>
2018-06-24 17:07:33 +0800

    add snapshot tool

---
+++ snapshot/README.md
@@ -0,0 +1,7 @@
+# Overview
+
+snapshot creates a snapshot of a blockchain by extracting all the unspent transaction outputs grouped by address
+
+snapshot accepts two parameters, the first of which is coin type, the second is the maximum block to be scanned.
+
+snapshot will create a CSV file that list all addresses with their balance.
---
+++ snapshot/main.go
@@ -0,0 +1,34 @@
+// Package snapshot prints out the whole book, by address
+//
+// Usage:
+//   snapshot <coin name> <max block heigth>
+// Example:
+//   snapshot shellcoin 100
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/hankgao/toolbox/src/skytb"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Printf(`Correct usage: 
+			snapshot <coin name> <max block height> 
+			for example: snapshot skycoin 10000
+			`)
+		return
+	}
+
+	coinName := os.Args[1]
+	maxBlock, err := strconv.Atoi(os.Args[2])
+
+	if err != nil {
+		fmt.Printf("Max block height has to be an integer\n")
+	}
+
+	skytb.Snapshot(coinName, maxBlock)
+}
