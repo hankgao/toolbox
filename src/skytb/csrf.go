73549181e17a2cca0c1d96604a16278494d084a6
Hank Gao <hankgao@gmail.com>
2018-07-07 19:22:06 +0800

    update skytb package

---
+++ src/skytb/csrf.go
@@ -0,0 +1,29 @@
+package skytb
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+// CsrfValue is something
+type CsrfValue struct {
+	CsrfToken string `json:"csrf_token"`
+}
+
+func getCsrfToken(coinName string) string {
+	csrf := CsrfValue{}
+	ctd := CoinTypeDetails(coinName)
+	resp, err := http.Get(fmt.Sprintf("http://127.0.0.1:%4d/csrf", ctd.WebInterfacePort))
+	if err != nil {
+		panic(err)
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+
+	json.Unmarshal(data, &csrf)
+
+	return csrf.CsrfToken
+
+}
---
+++ src/skytb/wallet.go
@@ -0,0 +1,119 @@
+package skytb
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+)
+
+// getWalletBalance returns balance (coins and hours) of a given wallet, coin type should be provided
+func getWalletBalance(wn, ct string) (uint64, uint64) {
+
+	ctd := CoinTypeDetails(ct)
+
+	resp, err := http.Get(fmt.Sprintf("http://127.0.0.1:%4d/wallet/balance?id=%s", ctd.WebInterfacePort, wn))
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	r := bufio.NewReader(resp.Body)
+
+	r.ReadString('\n')
+	r.ReadString('\n')
+	c, _ := r.ReadString('\n')
+	h, _ := r.ReadString('\n')
+
+	cs := strings.Split(c, ":")[1]
+	hs := strings.Split(h, ":")[1]
+
+	// rempve space, '\n', ','
+	cs = RemoveGarbage(cs)
+	hs = RemoveGarbage(hs)
+
+	coins, err := strconv.ParseUint(cs, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	hours, err := strconv.ParseUint(hs, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+
+	return coins, hours
+}
+
+// CreateWallet creates a wallet from a seed. You can give a name for the wallet, which will be used to replace the
+// system generated name
+func CreateWallet(cn, label, seed string) string {
+
+	v := url.Values{}
+	v.Set("seed", seed)
+	v.Set("label", label)
+	v.Set("scan", "1")
+
+	body := v.Encode()
+
+	ctd := CoinTypeDetails(cn)
+	req, err := http.NewRequest("POST", fmt.Sprintf("http://127.0.0.1:%d/wallet/create", ctd.WebInterfacePort), strings.NewReader(body))
+
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("X-CSRF-Token", getCsrfToken(cn))
+
+	c := &http.Client{}
+	resp, err := c.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	return fileName(string(data))
+}
+
+// CreateDistributionWallets creates 100 wallets from the initial distribution seeds
+func CreateDistributionWallets(cn string, seeds []string) []string {
+	wallets := make([]string, 100)
+
+	for i := 0; i < 100; i++ {
+		wallets[i] = CreateWallet(cn, fmt.Sprintf("distribution%03d", i), seeds[i])
+	}
+
+	return wallets
+}
+
+func fileName(js string) string {
+	fn := ""
+	r := bufio.NewReader(strings.NewReader(js))
+
+	for {
+		l, err := r.ReadString('\n')
+		if err != nil {
+			break
+		}
+
+		if strings.Contains(l, "filename") {
+			l = strings.Replace(l, "\"", "", -1)
+			l = strings.Replace(l, "filename", "", 1)
+			l = strings.Replace(l, ": ", "", 1)
+			l = strings.Replace(l, " ", "", -1)
+			fn = strings.Replace(l, ",", "", 1)
+			fmt.Printf("%s\n", fn)
+			break
+		}
+	}
+
+	return fn
+}
